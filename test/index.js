ba81903f9ae22982f13e2d69b49d397b18e6dc22
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-01-29 10:36:24 -0700

    Write tests that clicking a checkbox adds exercise to daily exercise table Write tests to check the correct sum for total calories on food and exercise diary logs

--- test/index.js
+++ test/index.js
@@ -1,64 +1,180 @@
-// var assert    = require('chai').assert;
-// var webdriver = require('selenium-webdriver');
-// var test      = require('selenium-webdriver/testing');
-// var expect = require('chai').expect;
-//
-// test.describe('testing diary', function() {
-//   var driver;
-//   this.timeout(10000);
-//
-//   test.beforeEach(function() {
-//     driver = new webdriver.Builder()
-//       .forBrowser('chrome')
-//       .build();
-//   })
-//
-//   test.afterEach(function() {
-//     driver.quit();
-//   })
-//
-//   test.xit('shows a table of all exercises', function(){
-//     driver.get('http://localhost:8080/exercises.html');
-//
-//     createExercise(driver, 'jumping', '100');
-//
-//     driver.get("http://localhost:8080/index.html");
-//     driver.executeScript("return window.localStorage.getItem('exercise-calories');")
-//     .then(function(exercisesCalories){
-//       assert.equal(exercisesCalories, JSON.stringify([{name: 'jumping', calories: '100'}]));
-//       driver.sleep(3000);
-//     });
-//   })
-//
-//   test.xit('adds a selected exercise to table', function(){
-//
-//     driver.get('http://localhost:8080/index.html');
-//     var calArray = JSON.stringify([{name: 'running', calories: '100'}]);
-//     driver.executeScript("window.localStorage.setItem('exercise-calories', '" + calArray + "');");
-//
-//     driver.sleep(3000);
-//
-//     var exerciseCheckbox = driver.findElement({css: 'diary-exercise-table tbody tr td:nth-of-type(3)'}).click();
-//     var addSelectedButton = driver.findElement({id: 'add-selected-exercise'});
-//
-//     addSelectedButton.click();
-//     driver.findElement({css: 'daily-exercise-table tbody tr td:nth-of-type(1)'})
-//     .getText()
-//     .then(function(textValue){
-//       assert.equal(textValue, 'running')
-//     });
-//
-//
-//   });
-//
-//   function createExercise(driver, nameKeys, calorieKeys){
-//     var name = driver.findElement({id: 'exercise-name'});
-//     var calories = driver.findElement({id: 'exercise-calories'});
-//     var submitButton = driver.findElement({id: 'add-exercise'});
-//
-//     name.sendKeys(nameKeys);
-//     calories.sendKeys(calorieKeys);
-//     submitButton.click();
-//   }
-//
-// });
+var assert    = require('chai').assert;
+var webdriver = require('selenium-webdriver');
+var test      = require('selenium-webdriver/testing');
+var expect = require('chai').expect;
+
+test.describe('testing diary', function() {
+  var driver;
+  this.timeout(10000);
+
+  test.beforeEach(function() {
+    driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .build();
+  })
+
+  test.afterEach(function() {
+    driver.quit();
+  })
+
+  test.xit('shows a table of all exercises', function(){
+    driver.get('http://localhost:8080/exercises.html');
+
+    createExercise(driver, 'jumping', '100');
+
+    driver.get("http://localhost:8080/index.html");
+    driver.executeScript("return window.localStorage.getItem('exercise-calories');")
+    .then(function(exercisesCalories){
+      assert.equal(exercisesCalories, JSON.stringify([{name: 'jumping', calories: '100'}]));
+      driver.sleep(3000);
+    });
+  })
+
+  test.xit('adds a selected exercise to table', function(){
+
+    driver.get('http://localhost:8080/index.html');
+    var calArray = JSON.stringify([{name: 'running', calories: '100'}]);
+    driver.executeScript("window.localStorage.setItem('exercise-calories', '" + calArray + "');");
+
+    driver.get('http://localhost:8080/index.html');
+
+    var exerciseCheckbox = driver.findElement({css: '#diary-exercise-body > tr > td:nth-child(3) > label'}).click();
+    var addSelectedButton = driver.findElement({id: 'add-selected-exercise'});
+
+    addSelectedButton.click();
+    driver.findElement({css: '#daily-exercise-body > tr:nth-child(1) > td:nth-child(1)'})
+    .getText()
+    .then(function(textValue){
+      assert.equal(textValue, 'running')
+    });
+  });
+
+  test.xit('adds the total calories eaten for breakfast', function(){
+
+    driver.get('http://localhost:8080/index.html');
+    var calArray = JSON.stringify([{name: 'apple', calories: '100'}, {name: 'pear', calories: '50'}]);
+    driver.executeScript("window.localStorage.setItem('daily-breakfast', '" + calArray + "');");
+
+    driver.get('http://localhost:8080/index.html');
+
+    driver.findElement({id: 'breakfast-total-calories'})
+    .getText()
+    .then(function(textValue){
+      assert.equal(textValue, '150')
+    });
+  });
+
+  test.it('adds the total calories eaten for breakfast', function(){
+
+    driver.get('http://localhost:8080/index.html');
+    var calArray = JSON.stringify([{name: 'apple', calories: '100'}, {name: 'pear', calories: '50'}]);
+    driver.executeScript("window.localStorage.setItem('daily-breakfast', '" + calArray + "');");
+
+    driver.get('http://localhost:8080/index.html');
+
+    driver.findElement({id: 'breakfast-total-calories'})
+    .getText()
+    .then(function(textValue){
+      assert.equal(textValue, '150')
+    });
+  });
+
+  test.xit('adds the total calories eaten for lunch', function(){
+
+    driver.get('http://localhost:8080/index.html');
+    var calArray = JSON.stringify([{name: 'sandwhich', calories: '200'}, {name: 'juice', calories: '150'}]);
+    driver.executeScript("window.localStorage.setItem('daily-lunch', '" + calArray + "');");
+
+    driver.get('http://localhost:8080/index.html');
+
+    driver.findElement({id: 'lunch-total-calories'})
+    .getText()
+    .then(function(textValue){
+      assert.equal(textValue, '350')
+    });
+  });
+
+  test.xit('adds the total calories eaten for dinner', function(){
+
+    driver.get('http://localhost:8080/index.html');
+    var calArray = JSON.stringify([{name: 'steak', calories: '600'}, {name: 'peas', calories: '50'}]);
+    driver.executeScript("window.localStorage.setItem('daily-dinner', '" + calArray + "');");
+
+    driver.get('http://localhost:8080/index.html');
+
+    driver.findElement({id: 'dinner-total-calories'})
+    .getText()
+    .then(function(textValue){
+      assert.equal(textValue, '650')
+    });
+  });
+
+  test.xit('adds the total calories eaten for snack', function(){
+
+    driver.get('http://localhost:8080/index.html');
+    var calArray = JSON.stringify([{name: 'oreos', calories: '300'}]);
+    driver.executeScript("window.localStorage.setItem('daily-snack', '" + calArray + "');");
+
+    driver.get('http://localhost:8080/index.html');
+
+    driver.findElement({id: 'snack-total-calories'})
+    .getText()
+    .then(function(textValue){
+      assert.equal(textValue, '300')
+    });
+  });
+
+  test.xit('adds the total calories burned during exercise', function(){
+
+    driver.get('http://localhost:8080/index.html');
+    var calArray = JSON.stringify([{name: 'running', calories: '100'}, {name: 'walk', calories: '50'}]);
+    driver.executeScript("window.localStorage.setItem('daily-exercise', '" + calArray + "');");
+
+    driver.get('http://localhost:8080/index.html');
+
+    driver.findElement({id: 'exercise-total-calories'})
+    .getText()
+    .then(function(textValue){
+      assert.equal(textValue, '150')
+    });
+  });
+
+  test.xit('redirects me to /foods.html when I click on the create new food button', function(){
+
+    driver.get('http://localhost:8080/index.html');
+
+    var newFoodButton = driver.findElement({id: 'create-food'});
+
+    newFoodButton.click().then(function() {
+        return driver.getCurrentUrl();
+    })
+    .then(function(currentUrl) {
+        assert.equal(currentUrl, 'http://localhost:8080/foods.html')
+    });
+  });
+
+  test.xit('redirects me to /exercises.html when I click on the create new food button', function(){
+
+    driver.get('http://localhost:8080/index.html');
+
+    var newFoodButton = driver.findElement({id: 'create-exercise'});
+
+    newFoodButton.click().then(function() {
+        return driver.getCurrentUrl();
+    })
+    .then(function(currentUrl) {
+        assert.equal(currentUrl, 'http://localhost:8080/exercises.html')
+    });
+  });
+
+  function createExercise(driver, nameKeys, calorieKeys){
+    var name = driver.findElement({id: 'exercise-name'});
+    var calories = driver.findElement({id: 'exercise-calories'});
+    var submitButton = driver.findElement({id: 'add-exercise'});
+
+    name.sendKeys(nameKeys);
+    calories.sendKeys(calorieKeys);
+    submitButton.click();
+  }
+
+});
