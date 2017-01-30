3e195802b0d91f37d86132707f8c07e61626e52d
Daniel Shin <shin.daniel.d@gmail.com>
2017-01-29 21:16:16 -0700

    massive refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 module.exports = {
   entry: {
     main: "./lib/index.js",
-    test: "mocha!./test/index.js"
+    test: "mocha!./test/index.js",
+    exercises: "./lib/exercise_render.js",
+    foods: "./lib/food_render.js",
   },
   output: {
     path: __dirname,
